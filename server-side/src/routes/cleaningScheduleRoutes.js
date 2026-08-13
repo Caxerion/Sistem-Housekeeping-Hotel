a02698e1190410832cc65d8e3ad363641bfa4a6e
Caxerion <kennethgeveny1@gmail.com>
2026-08-13 23:28:07 +0700

    chore(feat): added supervisor inspection for penugasan

--- server-side/src/routes/cleaningScheduleRoutes.js
+++ server-side/src/routes/cleaningScheduleRoutes.js
@@ -8,6 +8,8 @@ const {
     updateCleaningSchedule,
     startCleaningSchedule,
     completeCleaningSchedule,
+    inspectCleaningSchedule,
+    revertCleaningSubmission,
     cancelCleaningSchedule,
     assignStaffToCleaningSchedule,
     uploadCleaningPhotos,
@@ -31,6 +33,8 @@ router.post('/:id/upload-photos', uploadMaintenancePhotos.array('photos', 20), u
 router.delete('/:id/photos', deleteCleaningPhoto);
 router.put('/:id/start', startCleaningSchedule);
 router.put('/:id/complete', completeCleaningSchedule);
+router.put('/:id/inspect', inspectCleaningSchedule);
+router.post('/:id/revert-submit', revertCleaningSubmission);
 router.put('/:id/cancel', cancelCleaningSchedule);
 
 module.exports = router;
